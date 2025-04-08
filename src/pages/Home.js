effed7c96986303b718f1a08cb2f1291d587b6c2
Amar Mapare <118011011+Amar41901@users.noreply.github.com>
2025-04-09 01:21:20 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import '../styles/Home.css';
 const Home = () => {
     return (
         <>
-            <img height="400px" width="100%" src='https://media.istockphoto.com/id/482858629/photo/doctors-hospital-corridor-nurse-pushing-gurney-stretcher-bed.jpg?s=612x612&w=0&k=20&c=unfa1VMpYQGt3PyrkuvxN1JkX7FRk-w0knEFCqjTugg='></img>
+            <img height="400px" width="100%" alt="homepageimage" src='https://media.istockphoto.com/id/482858629/photo/doctors-hospital-corridor-nurse-pushing-gurney-stretcher-bed.jpg?s=612x612&w=0&k=20&c=unfa1VMpYQGt3PyrkuvxN1JkX7FRk-w0knEFCqjTugg='></img>
             <div className="home-container">
             <h1 className="welcome-title">Welcome to Our Mapro Hospital</h1>
             <p className="welcome-message">
